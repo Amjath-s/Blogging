0b7606923f602807d5cca6b5f38cffe0ecc76f57
Amjath s <amjaths788@gmail.com>
2025-09-13 03:22:14 +0530

    correction on Footer

--- src/component/index.js
+++ src/component/index.js
@@ -1,5 +1,5 @@
 import Header from "./Header/Header";
-import Footer from "./footer/Footer";
+import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
