73b197b073c61b5bca0bdc9089a282978ff7aa4b
Amjath s <amjaths788@gmail.com>
2025-09-14 20:35:35 +0530

    redirectulr change

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -2,6 +2,7 @@ import { use } from "react";
 import config from "../conf/config";
 import authorService from "./authorService";
 import { Client, Account, ID } from "appwrite";
+import { redirect } from "react-router-dom";
 
 export class AuthService {
   client = new Client();
@@ -67,15 +68,16 @@ export class AuthService {
       return true;
     }
   }
-    async loginWithGoogle(redirectUrl = window.location.origin) {
-      console.log("this thingis wokring or not ")
+    async loginWithGoogle() {
+     
       try { 
         // This will redirect the user to Google for authentication
+        redirectUrl = window.location.origin;
         return await this.account.createOAuth2Session(
           "google",
-          "http://localhost:5173/", // Success redirect URL
+          redirectUrl, // Success redirect URL
           "http://localhost:5173/" // Failure redirect URL
-        )
+        );
   
 
         
